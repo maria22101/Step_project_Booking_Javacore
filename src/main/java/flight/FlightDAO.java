b5b8f2dc6e4e29f306c393f5c68705ea036c0167
Maria101 <maria.bilous101@gmail.com>
2019-10-28 22:09:12 +0200

    FlightDAO interface - methods defined

--- src/main/java/flight/FlightDAO.java
+++ src/main/java/flight/FlightDAO.java
@@ -1,4 +1,11 @@
 package flight;
 
+import java.util.List;
+
 public interface FlightDAO {
+    void createFlight(String flightId, String destination, long departureTime, int freeSeats);
+    List<Flight> getAll();
+    Flight getFlightById(int id);
+    boolean deleteFlight(int flightToDeleteId);
+    boolean deleteFlight(Flight flightToDelete);
 }
