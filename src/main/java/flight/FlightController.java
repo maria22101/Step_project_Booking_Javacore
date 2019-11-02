12d1db4e5cae947b4102eec48d4c798ec19f268b
Maria101 <maria.bilous101@gmail.com>
2019-11-02 15:12:26 +0200

    FlightController drafted

--- src/main/java/flight/FlightController.java
+++ src/main/java/flight/FlightController.java
@@ -1,5 +1,39 @@
 package flight;
 
+import java.util.List;
+
 public class FlightController {
     private FlightService flightService = new FlightService();
+
+    public void createNewFlight(String flightId, String destination, long departureTime, int freeSeats) {
+        flightService.createNewFlight(flightId, destination, departureTime, freeSeats);
+    }
+
+    public List<Flight> getAllFlights() {
+        return flightService.getAllFlights();
+    }
+
+    public Flight getFlightById(int id) {
+        return flightService.getFlightById(id);
+    }
+
+    public boolean deleteFlightById(int id) {
+        return flightService.deleteFlightById(id);
+    }
+
+    public boolean deleteFlight(Flight flight) {
+        return flightService.deleteFlight(flight);
+    }
+
+    void displayAllFlights() {
+        flightService.displayAllFlights();
+    }
+
+    public List<Flight> findFlights(String destination, long departureTime, int passengersNumber) {
+        return flightService.findFlights(destination, departureTime, passengersNumber);
+    }
+
+    public void displayFoundFlights(String destination, long departureTime, int passengersNumber) {
+        flightService.displayFoundFlights(destination, departureTime, passengersNumber);
+    }
 }
--- src/main/java/flight/FlightService.java
+++ src/main/java/flight/FlightService.java
@@ -1,6 +1,5 @@
 package flight;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
